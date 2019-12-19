e34ae6c50a3025a0c01daede9ee5a0d997de2e54
Shuoliu Yang <yangshuoliu@sina.com>
2019-12-19 23:48:41 +0800

    fix

--- include/VMUtils/common.h
+++ include/VMUtils/common.h
@@ -16,7 +16,7 @@ static inline typename Wrapper::pointer GetImplPtrHelper( const Wrapper &p )
 	inline Class##__pImpl *d_func() { return reinterpret_cast<Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); }                   \
 	inline const Class##__pImpl *d_func() const { return reinterpret_cast<const Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); } \
 	friend class Class##__pImpl; \
-	std::unique_ptr<Class##__pImpl *> const d_ptr;
+	std::unique_ptr<Class##__pImpl> const d_ptr;
 
 #define VM_DECL_API( Class )                                                           \
 	inline Class *q_func() { return static_cast<Class *>( q_ptr ); }                   \
