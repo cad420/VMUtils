9707940b1bc8fabc5b2029d26fe2a110da903854
Shuoliu Yang <yangshuoliu@sina.com>
2019-12-19 21:25:11 +0800

    add impl macro

--- include/VMUtils/common.h
+++ include/VMUtils/common.h
@@ -1,7 +1,33 @@
 
-#ifndef _COMMON_H_
+#pragma once
 
-// empty now
+template <typename T>
+static inline T *GetImplPtrHelper( T *ptr )
+{
+	return ptr;
+}
+template <typename Wrapper>
+static inline typename Wrapper::pointer GetImplPtrHelper( const Wrapper &p )
+{
+	return p.data();
+}
 
-#define _COMMON_H_
-#endif
+#define VM_DECL_IMPL( Class )                                                                                             \
+	inline Class##__pImpl *d_func() { return reinterpret_cast<Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); }                   \
+	inline const Class##__pImpl *d_func() const { return reinterpret_cast<const Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); } \
+	friend class Class##__pImpl; \
+	std::unique_ptr<Class##__pImpl> d_ptr;
+
+#define VM_DECL_IMPL_D( Dptr, Class )                                                                                    \
+	inline Class##__pImpl *d_func() { return reinterpret_cast<Class##__pImpl *>( GetImplPtrHelper( Dptr ) ); }                   \
+	inline const Class##__pImpl *d_func() const { return reinterpret_cast<const Class##__pImpl *>( GetImplPtrHelper( Dptr ) ); } \
+	friend class Class##__pImpl;
+
+#define VM_DECL_API( Class )                                                      \
+	inline Class *q_func() { return static_cast<Class *>( q_ptr ); }                   \
+	inline const Class *q_func() const { return static_cast<const Class *>( q_ptr ); } \
+	friend class Class;\
+    Class * const q_ptr = nullptr;
+
+#define VM_IMPL( Class ) Class##__pImpl *const _ = d_func();
+#define VM_API ( Class ) Class * const _ = q_func();
