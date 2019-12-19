027243aa3009dde99d7f3ec6b87fb18473dc759d
Shuoliu Yang <yangshuoliu@sina.com>
2019-12-19 23:25:42 +0800

    fix

--- include/VMUtils/common.h
+++ include/VMUtils/common.h
@@ -15,18 +15,14 @@ static inline typename Wrapper::pointer GetImplPtrHelper( const Wrapper &p )
 #define VM_DECL_IMPL( Class )                                                                                             \
 	inline Class##__pImpl *d_func() { return reinterpret_cast<Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); }                   \
 	inline const Class##__pImpl *d_func() const { return reinterpret_cast<const Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); } \
-	friend class Class##__pImpl; 
-	//Class##__pImpl * const d_ptr = nullptr;
+	friend class Class##__pImpl; \
+	std::unique_ptr<Class##__pImpl *> const d_ptr;
 
-#define VM_DECL_IMPL_D( Dptr, Class )                                                                                    \
-	inline Class##__pImpl *d_func() { return reinterpret_cast<Class##__pImpl *>( GetImplPtrHelper( Dptr ) ); }                   \
-	inline const Class##__pImpl *d_func() const { return reinterpret_cast<const Class##__pImpl *>( GetImplPtrHelper( Dptr ) ); } \
-	friend class Class##__pImpl;
-
-#define VM_DECL_API( Class )                                                      \
+#define VM_DECL_API( Class )                                                           \
 	inline Class *q_func() { return static_cast<Class *>( q_ptr ); }                   \
 	inline const Class *q_func() const { return static_cast<const Class *>( q_ptr ); } \
-	friend class Class;
+	friend class Class; \
+	Class * const q_ptr = nullptr;
 
 #define VM_IMPL( Class ) Class##__pImpl *const _ = d_func();
 #define VM_API ( Class ) Class * const _ = q_func();
