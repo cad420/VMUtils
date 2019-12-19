85b70a25acabf20c33e363add13fb2b64629df7e
Shuoliu Yang <yangshuoliu@sina.com>
2019-12-19 23:02:49 +0800

    fix

--- include/VMUtils/common.h
+++ include/VMUtils/common.h
@@ -15,8 +15,8 @@ static inline typename Wrapper::pointer GetImplPtrHelper( const Wrapper &p )
 #define VM_DECL_IMPL( Class )                                                                                             \
 	inline Class##__pImpl *d_func() { return reinterpret_cast<Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); }                   \
 	inline const Class##__pImpl *d_func() const { return reinterpret_cast<const Class##__pImpl *>( GetImplPtrHelper( d_ptr.get() ) ); } \
-	friend class Class##__pImpl; \
-	Class##__pImpl * const d_ptr = nullptr;
+	friend class Class##__pImpl; 
+	//Class##__pImpl * const d_ptr = nullptr;
 
 #define VM_DECL_IMPL_D( Dptr, Class )                                                                                    \
 	inline Class##__pImpl *d_func() { return reinterpret_cast<Class##__pImpl *>( GetImplPtrHelper( Dptr ) ); }                   \
@@ -26,8 +26,8 @@ static inline typename Wrapper::pointer GetImplPtrHelper( const Wrapper &p )
 #define VM_DECL_API( Class )                                                      \
 	inline Class *q_func() { return static_cast<Class *>( q_ptr ); }                   \
 	inline const Class *q_func() const { return static_cast<const Class *>( q_ptr ); } \
-	friend class Class;\
-    Class * const q_ptr = nullptr;
+	friend class Class;
+    //Class * const q_ptr = nullptr;
 
 #define VM_IMPL( Class ) Class##__pImpl *const _ = d_func();
 #define VM_API ( Class ) Class * const _ = q_func();
