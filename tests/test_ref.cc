d4688f2dbba8464e04c51c4fc78100111fb7c684
Yang Shuoliu <yangshuoliu@sina.com>
2020-02-03 17:23:22 +0800

    comment ref

--- tests/test_ref.cc
+++ tests/test_ref.cc
@@ -5,30 +5,30 @@
 #include <VMUtils/weakref.hpp>
 
 
-using namespace vm;
-using namespace std;
-
-vector<int> x;
-
-struct MyAllocator : IAllocator
-{
-	void *Alloc( size_t size ) override
-	{
-		x.emplace_back( 1 );
-		return new uint8_t[ size ];
-	}
-	void Free( void *ptr ) override
-	{
-		x.emplace_back( 2 );
-		delete[] reinterpret_cast<uint8_t *>( ptr );
-	}
-};
-
-// Define an interface based IEverything
-struct A : IEverything
-{
-	virtual void Foo() = 0;
-};
+//using namespace vm;
+//using namespace std;
+//
+//vector<int> x;
+//
+//struct MyAllocator : IAllocator
+//{
+//	void *Alloc( size_t size ) override
+//	{
+//		x.emplace_back( 1 );
+//		return new uint8_t[ size ];
+//	}
+//	void Free( void *ptr ) override
+//	{
+//		x.emplace_back( 2 );
+//		delete[] reinterpret_cast<uint8_t *>( ptr );
+//	}
+//};
+//
+//// Define an interface based IEverything
+//struct A : IEverything
+//{
+//	virtual void Foo() = 0;
+//};
 
 //void *operator new( size_t size )
 //{
@@ -43,60 +43,60 @@ struct A : IEverything
 //}
 
 // Actual implementation
-class AImpl : public EverythingBase<A>
-{
-	int a = 0;
-	const char *info = nullptr;
-
-public:
-	AImpl( IRefCnt *cnt, int a ,const char * str = nullptr) :
-	  EverythingBase<A>( cnt ), a( a ),info(str)	// The first parameter of the ctor must be IRefCnt*
-	{
-		cout << info << " has been created\n";
-	}
-	void Foo() override
-	{
-		cout << a << " "
-			 << "Oops\n";
-	}
-	~AImpl()noexcept
-	{
-		cout << info << " has been destroyed\n";
-	}
-};
-
-TEST( test_ref, test_ref )
-{
-	{
-		std::cout << "sizeof(AImpl) is: " << sizeof( AImpl ) << std::endl;
-		auto p = VM_NEW<AImpl>( 1,"p" );
-		Ref<A> a = p;
-		Ref<A> b( p );
-		Ref<A> c( a );
-		Ref<A> d( c.Get() );
-
-		MyAllocator myAlloc;
-		Ref<A> e = VM_NEW<MyAllocator, AImpl>( &myAlloc, 1,"e" );
-	}
-	EXPECT_EQ( x, ( vector<int>{ 1, 2 } ) );
-}
-
-TEST( test_ref, WeakRef )
-{
-	WeakRef<AImpl> wp1, wp2;
-	{
-		auto p = VM_NEW<AImpl>( 1 );
-		Ref<AImpl> sp = p;
-		wp1 = sp;
-		wp2 = p;
-		EXPECT_FALSE( wp1.Expired());
-		EXPECT_FALSE( wp2.Expired());
-
-		auto sp1 = wp1.Lock();
-		EXPECT_TRUE( sp1.Get() != nullptr );
-	}
-
-	EXPECT_TRUE( wp1.Expired() );
-	EXPECT_TRUE( wp2.Expired() );
-
-}
+//class AImpl : public EverythingBase<A>
+//{
+//	int a = 0;
+//	const char *info = nullptr;
+//
+//public:
+//	AImpl( IRefCnt *cnt, int a ,const char * str = nullptr) :
+//	  EverythingBase<A>( cnt ), a( a ),info(str)	// The first parameter of the ctor must be IRefCnt*
+//	{
+//		cout << info << " has been created\n";
+//	}
+//	void Foo() override
+//	{
+//		cout << a << " "
+//			 << "Oops\n";
+//	}
+//	~AImpl()noexcept
+//	{
+//		cout << info << " has been destroyed\n";
+//	}
+//};
+//
+//TEST( test_ref, test_ref )
+//{
+//	{
+//		std::cout << "sizeof(AImpl) is: " << sizeof( AImpl ) << std::endl;
+//		auto p = VM_NEW<AImpl>( 1,"p" );
+//		Ref<A> a = p;
+//		Ref<A> b( p );
+//		Ref<A> c( a );
+//		Ref<A> d( c.Get() );
+//
+//		MyAllocator myAlloc;
+//		Ref<A> e = VM_NEW<MyAllocator, AImpl>( &myAlloc, 1,"e" );
+//	}
+//	EXPECT_EQ( x, ( vector<int>{ 1, 2 } ) );
+//}
+//
+//TEST( test_ref, WeakRef )
+//{
+//	WeakRef<AImpl> wp1, wp2;
+//	{
+//		auto p = VM_NEW<AImpl>( 1 );
+//		Ref<AImpl> sp = p;
+//		wp1 = sp;
+//		wp2 = p;
+//		EXPECT_FALSE( wp1.Expired());
+//		EXPECT_FALSE( wp2.Expired());
+//
+//		auto sp1 = wp1.Lock();
+//		EXPECT_TRUE( sp1.Get() != nullptr );
+//	}
+//
+//	EXPECT_TRUE( wp1.Expired() );
+//	EXPECT_TRUE( wp2.Expired() );
+//
+//}
