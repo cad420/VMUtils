55e73f07cc9e06096e2e3e991826d82639f646f3
ukabuer <ukabuer@live.com>
2020-04-19 23:12:45 +0800

    add CMake install target

--- export.cmake
+++ export.cmake
@@ -1,6 +1,16 @@
 add_library(VMUtils INTERFACE)
 
-target_include_directories(VMUtils INTERFACE 
-  ${CMAKE_CURRENT_SOURCE_DIR}/include
+target_include_directories(VMUtils
+  INTERFACE 
+    $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+    $<INSTALL_INTERFACE:include>
 )
+target_compile_features(VMUtils INTERFACE cxx_std_14)
 
+option(VMUTILS_INSTALL "install VMUtils headers" ON)
+if (VMUTILS_INSTALL)
+  install(
+    DIRECTORY include/VMUtils
+    DESTINATION include
+  )
+endif()
